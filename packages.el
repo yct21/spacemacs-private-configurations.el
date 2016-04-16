031c077e697e7a8982f65b79b01717a9bc0e9ef1
Chutian Yang <yct21@12tcy.com>
2016-04-16 09:01:59 +0800

    Initialize packages.el.

---
+++ packages.el
@@ -0,0 +1,14 @@
+;;; packages.el --- spacemeow layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Chutian Yang <yct21@12tcy.com>
+;; URL: https://github.com/yct21/spacemeow
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst spacemeow-packages
+  '()
+)
