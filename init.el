5e6ed1eee4e976ddc93d329d7330a865165f72e2
Chutian Yang <yct21@12tcy.com>
2016-05-09 17:58:09 +0800

    Change auto completion setting

--- init.el
+++ init.el
@@ -24,7 +24,11 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-enable-help-tooltip t
+                      auto-completion-enable-sort-by-usage t
+                      auto-completion-enable-snippets-in-popup t
+                      )
      emacs-lisp
      git
      dash
