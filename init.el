d3c44bc9153f041cc0b0c44042ab8d699c66d956
Chutian Yang <yct21@12tcy.com>
2016-05-14 18:08:47 +0800

    Fix layer configuration of auto-completion

--- init.el
+++ init.el
@@ -27,8 +27,8 @@ values."
      (auto-completion :variables
                       auto-completion-enable-help-tooltip t
                       auto-completion-enable-sort-by-usage t
-                      auto-completion-enable-snippets-in-popup t
-                      )
+                      auto-completion-enable-snippets-in-popup t)
+
      emacs-lisp
      git
      dash
