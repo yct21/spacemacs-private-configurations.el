102076e3b8e4294b6a82d46bec7559f57da4a82c
Chutian Yang <yct21@12tcy.com>
2016-05-18 17:42:32 +0800

    Wrap wakatime initializer with use-package macro.

--- wakatime-config.el
+++ wakatime-config.el
@@ -1,4 +1,7 @@
 (defun spacemeow/init-wakatime-mode ()
-  (setq wakatime-api-key  "44bcd124-1fb7-44a3-a7b6-aae4fecc969d"
-        wakatime-cli-path "/usr/local/bin/wakatime")
-  (global-wakatime-mode t))
+  (use-package wakatime-mode
+    :defer t
+    :init
+    (setq wakatime-api-key  "44bcd124-1fb7-44a3-a7b6-aae4fecc969d"
+          wakatime-cli-path "/usr/local/bin/wakatime")
+    (global-wakatime-mode t)))
