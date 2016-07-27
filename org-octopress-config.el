857ea5150efb97370ea40502b25c81365c2a9383
Chutian Yang <yct21@12tcy.com>
2016-07-27 11:21:08 +0800

    Add keybinding for org-export-dispatch.

--- org-octopress-config.el
+++ org-octopress-config.el
@@ -7,5 +7,6 @@
       (setq org-octopress-directory-posts "~/12tcy/source/_posts")
       (setq org-octopress-directory-org-top "~/12tcy/source")
       (setq org-octopress-directory-org-posts "~/12tcy/source/_orgs")
-      (setq org-octopress-setup-file "~/12tcy/setupfile.org"))
+      (setq org-octopress-setup-file "~/12tcy/setupfile.org")
+      (spacemacs/set-leader-keys-for-major-mode 'org-mode "oe" 'org-export-dispatch))
   )
