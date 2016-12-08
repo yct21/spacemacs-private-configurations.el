b9c0697d0105b8456a08ec84b35e873df8c5b1c4
Chutian Yang <yct21@12tcy.com>
2016-12-08 21:18:30 +0800

    Remove customized eslint-executable and add hook to find correct eslint.

--- package-configurations/react-config.el
+++ package-configurations/react-config.el
@@ -1,7 +1,23 @@
 (defun spacemeow/post-init-flycheck ()
   (with-eval-after-load 'flycheck
-    (setq flycheck-javascript-eslint-executable "eslint-project-relative")))
+    (add-hook 'flycheck-mode-hook #'spacemeow//use-eslint-from-node-modules)))
+
+(defun spacemeow/init-emmet-mode)
 
 (defun spacemeow/post-init-emmet-mode ()
   (with-eval-after-load 'emmet-mode
     (setq emmet-self-closing-tag-style " /")))
+
+(defun spacemeow//use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root)))
+         (eslintrc (and root
+                        (expand-file-name ".eslintrc.js"
+                                          root))))
+    (when (and eslint (file-executable-p eslint) (file-exists-p eslintrc))
+      (setq-local flycheck-javascript-eslint-executable eslint)
+      (setq-local flycheck-eslintrc eslintrc))))
