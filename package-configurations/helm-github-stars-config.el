0c6bd9d91f7e907b7202c492b49e85482482555a
Chutian Yang <yct21@12tcy.com>
2017-01-11 13:44:52 +0800

    Stop defering helm-github-stars.

--- package-configurations/helm-github-stars-config.el
+++ package-configurations/helm-github-stars-config.el
@@ -1,6 +1,6 @@
 (defun spacemeow/init-helm-github-stars ()
   (use-package helm-github-stars
-    :defer
+    :defer nil
     :commands helm-github-stars
     :config
     (setq helm-github-stars-username "yct21")
