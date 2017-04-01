f7de3e2f2fada6c77e110d37f6925ba625c53fbb
Chutian Yang <yct21@12tcy.com>
2017-04-01 11:27:12 +0800

    define key "T" to evil-avy-goto-line

--- package-configurations/evil-config.el
+++ package-configurations/evil-config.el
@@ -2,6 +2,8 @@
   (with-eval-after-load 'evil
     (define-key evil-motion-state-map "t" 'evil-avy-goto-char)
     (define-key evil-normal-state-map "t" 'evil-avy-goto-char)
+    (define-key evil-motion-state-map "T" 'evil-avy-goto-line)
+    (define-key evil-normal-state-map "T" 'evil-avy-goto-line)
     (with-eval-after-load 'org
       (evil-define-key 'normal evil-org-mode-map "t" nil)
-      (evil-define-key 'normal evil-org-mode-map "T" 'org-todo))))
+      (evil-define-key 'normal evil-org-mode-map "f" 'org-todo))))
