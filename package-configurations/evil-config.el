4ceb2ac205c28fc4a27cd63f5ac72970b05ab373
Chutian Yang <yct21@12tcy.com>
2017-11-30 15:42:30 +0800

    Use visual line move in Evil

--- package-configurations/evil-config.el
+++ package-configurations/evil-config.el
@@ -4,6 +4,12 @@
     (define-key evil-normal-state-map "t" 'evil-avy-goto-char)
     (define-key evil-motion-state-map "T" 'evil-avy-goto-line)
     (define-key evil-normal-state-map "T" 'evil-avy-goto-line)
+    ;; Make movement keys work like they should
+    (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+    (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+    (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+    (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+    (setq-default evil-cross-lines t)
     (with-eval-after-load 'org
       (evil-define-key 'normal evil-org-mode-map "t" nil)
       (evil-define-key 'normal evil-org-mode-map "f" 'org-todo))))
